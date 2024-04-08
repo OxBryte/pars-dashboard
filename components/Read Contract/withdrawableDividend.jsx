d08d8b4c17634a0510e4b77679e61ad2a5242f71
BrightTechnologies1 <silascyrax@gmail.com>
2024-04-08 13:24:04 +0100

    update

--- components/Read Contract/withdrawableDividend.jsx
+++ components/Read Contract/withdrawableDividend.jsx
@@ -3,7 +3,7 @@ import { useAddress } from "@thirdweb-dev/react";
 import { useEffect, useState, useCallback } from "react";
 
 export default function WithdrawableDividend({ customAddress }) {
-  const address = customAddress || useAddress();
+  const address = useAddress();
   const { contract } = useContract("0x593649F70f836565e33f0BCe9af9503c243359B3");
   const { data: balance, refetch: refetchBalance, isLoading, error } = useContractRead(contract, "withdrawableDividendOf", [address]);
 
